4efb45e7dd64c870950608bc4132b3b560338462
Ben Tefay <btefay@gmail.com>
2015-02-18 09:41:22 +1000

    Changes to adx and more tests

--- Shares.Model/Indicators/Adx.cs
+++ Shares.Model/Indicators/Adx.cs
@@ -10,40 +10,36 @@ namespace Shares.Model.Indicators
 {
     public class Adx
     {
-        /* 
-        The ADX is a combination of two other indicators developed by Wilder, the positive directional indicator (abbreviated +DI) and negative directional indicator (-DI).
-        The ADX combines them and smooths the result with an exponential moving average.
-
-        To calculate +DI and −DI, one needs price data consisting of high, low, and closing prices each period (typically each day). 
-        One first calculates the directional movement (+DM and −DM):
-
-        UpMove = today's high − yesterday's high
-        DownMove = yesterday's low − today's low
-        if UpMove > DownMove and UpMove > 0, then +DM = UpMove, else +DM = 0
-        if DownMove > UpMove and DownMove > 0, then −DM = DownMove, else −DM = 0
-        After selecting the number of periods (Wilder used 14 days originally), +DI and −DI are:
-
-        +DI = 100 times exponential moving average of (+DM) divided by average true range
-        −DI = 100 times exponential moving average of (−DM) divided by average true range
-        The exponential moving average is calculated over the number of periods selected, and the average true range is an exponential average of the true ranges. Then:
-
-        ADX = 100 times the exponential moving average of the absolute value of (+DI − −DI) divided by (+DI + −DI)
-        */
-
-        public IEnumerable<Point<decimal>> Calculate(ShareDay[] days, Parameters p)
+        /// <summary>
+        /// The ADX (Average Directional Index) is a combination of two other indicators developed by Wilder:
+        /// - the positive directional indicator (+DI) 
+        /// - and the negative directional indicator (-DI).
+        ///  
+        /// The ADX combines them and smooths the result with an exponential moving average.
+        /// 
+        /// First, calculate directional movement values (+DM and −DM):
+        /// 
+        /// UpMove = today's high − yesterday's high
+        /// DownMove = yesterday's low − today's low
+        /// if UpMove > DownMove and UpMove > 0, then +DM = UpMove, else +DM = 0
+        /// if DownMove > UpMove and DownMove > 0, then −DM = DownMove, else −DM = 0
+        /// 
+        /// Using a fixed number of periods (Wilder used 14 days originally) for the ATR and EMA:
+        /// 
+        /// +DI = 100 times exponential moving average of (+DM) divided by average true range
+        /// −DI = 100 times exponential moving average of (−DM) divided by average true range
+        /// ADX = 100 times the exponential moving average of the absolute value of (+DI − −DI) divided by (+DI + −DI)
+        /// 
+        /// </summary>
+        public IEnumerable<Point> Calculate(ShareDay[] days, Parameters p)
         {
             return Calculate(days, p.Periods);
-        }
-
-        public static IEnumerable<Point<decimal>> Calculate(ShareDay[] days, int shortPeriods)
-        {
-            throw new Exception();
         }
 
         /// <summary>
         /// Returns directional movement array of length [days.Length - 1]
         /// </summary>
-        public static IEnumerable<DirectionalMovementPoint> DirectionalMovement(ShareDay[] days)
+        public static IEnumerable<DirectionalMovementPoint> CalculateDirectionalMovement(ShareDay[] days)
         {
             for (int i = 1; i < days.Length; i++)
             {
@@ -55,36 +51,55 @@ namespace Shares.Model.Indicators
             }
         }
 
-        public static IEnumerable<Point<Decimal>> DirectionalIndicator(ShareDay[] days, int periods)
+        public static IEnumerable<Point> Calculate(ShareDay[] days, int periods)
         {
-            var dmPoints = DirectionalMovement(days).ToArray();
+            var dmPoints = CalculateDirectionalMovement(days).ToArray();
             var positiveDmEma = Ema.Calculate(dmPoints, p => p.DateTime, p => p.PositiveDm, periods).ToArray();
             var negativeDmEma = Ema.Calculate(dmPoints, p => p.DateTime, p => p.NegativeDm, periods).ToArray();
             var atr = Atr.Calculate(days, periods, 0, false).ToArray();
-            var positiveDi = positiveDmEma.ZipEnds(atr, (ema, a) => Point.With(ema.DateTime, 100 * ema.Value / a.Value));
-            var negativeDi = negativeDmEma.ZipEnds(atr, (ema, a) => Point.With(ema.DateTime, 100 * ema.Value / a.Value));
-            var preAdx = positiveDi.Zip(negativeDi, (p, n) => Point.With(p.DateTime, 100 * Math.Abs(p.Value - n.Value) / Math.Abs(p.Value + n.Value))).ToArray();
-            var adx = Ema.Calculate(preAdx, p => p.DateTime, p => p.Value, periods);
-            return adx;
+            var positiveDi = positiveDmEma.ZipEnds(atr, (ema, a) => Model.Point.With(ema.DateTime, 100 * ema.Value / a.Value)).ToArray();
+            var negativeDi = negativeDmEma.ZipEnds(atr, (ema, a) => Model.Point.With(ema.DateTime, 100 * ema.Value / a.Value)).ToArray();
+            var preAdx = positiveDi.Zip(negativeDi, (p, n) => Model.Point.With(p.DateTime, 
+                100 * Math.Abs(p.Value - n.Value) / Math.Abs(p.Value + n.Value))).ToArray();
+            var adx = Ema.Calculate(preAdx, p => p.DateTime, p => p.Value, periods).ToArray();
+
+            for (int i = 0, j = periods - 1; i < adx.Length; i++, j++)
+            {
+                yield return new Point
+                {
+                    DateTime = adx[i].DateTime, 
+                    Adx = adx[i].Value, 
+                    NegativeDi = negativeDi[j].Value, 
+                    PositiveDi = positiveDi[j].Value
+                };
+            }
         }
 
         public class Parameters
         {
             public Parameters()
             {
-                Periods = 5;
+                Periods = 14;
             }
 
-            [Description("The number of periods to include in the moving average.")]
+            [Description("The number of periods to include in the moving averages.")]
             [DisplayName("Periods")]
             public int Periods { get; set; }
         }
-    }
 
-    public class DirectionalMovementPoint
-    {
-        public DateTime DateTime { get; set; }
-        public decimal PositiveDm { get; set; }
-        public decimal NegativeDm { get; set; }
+        public class DirectionalMovementPoint
+        {
+            public DateTime DateTime { get; set; }
+            public decimal PositiveDm { get; set; }
+            public decimal NegativeDm { get; set; }
+        }
+
+        public class Point
+        {
+            public DateTime DateTime { get; set; }
+            public Decimal PositiveDi { get; set; }
+            public Decimal NegativeDi { get; set; }
+            public Decimal Adx { get; set; }
+        }
     }
 }
---
+++ Shares.Model/ReadOnlyListExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shares.Model
+{
+    public static class ReadOnlyListExtensions
+    {
+        public static IEnumerable<TOut> ZipEnds<TIn, TOut>(this IReadOnlyList<TIn> t1, IReadOnlyList<TIn> t2, Func<TIn, TIn, TOut> combine)
+        {
+            IEnumerable<TIn> t1Enumerable;
+            IEnumerable<TIn> t2Enumerable;
+
+            if (t1.Count > t2.Count)
+            {
+                var difference = t1.Count - t2.Count;
+                t1Enumerable = t1.Skip(difference);
+                t2Enumerable = t2;
+            }
+            else
+            {
+                var difference = t2.Count - t1.Count;
+                t1Enumerable = t1;
+                t2Enumerable = t2.Skip(difference);
+                
+            }
+            return t1Enumerable.Zip(t2Enumerable, combine);
+        }
+
+        public static IEnumerable<T> Skip<T>(this IReadOnlyList<T> source, int count)
+        {
+            for (int i = count; i < source.Count; i++)
+                yield return source[i];
+        }
+    }
+}
