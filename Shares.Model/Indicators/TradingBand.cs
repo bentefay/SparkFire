0d5e12d088ff82f684483fe1980966bbf4763059
singularity-zero <btefay@gmail.com>
2015-03-08 10:31:21 +1000

    Completed trading band indicator

--- Shares.Model/Indicators/TradingBand.cs
+++ Shares.Model/Indicators/TradingBand.cs
@@ -1,22 +1,39 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 
 namespace Shares.Model.Indicators
 {
     public class TradingBand
     {
-        public IEnumerable<Point<decimal>> Calculate(ShareDay[] days, Parameters p)
+        public IEnumerable<Point> Calculate(ShareDay[] days, Parameters p)
         {
-            return Calculate(days, p.Periods);
+            return Calculate(days, p.Periods, p.PlusMinusPercentage);
         }
 
         /// <summary>
         /// Returns trading band array of length [days.Length - periods + 1]
         /// </summary>
-        public static IEnumerable<Point<decimal>> Calculate(ShareDay[] days, int periods)
+        public static IEnumerable<Point> Calculate(ShareDay[] days, int periods, decimal plusMinusPercentage)
         {
             var sma = Sma.Calculate(days, periods);
-            return sma;
+            var tradingBand = sma.Select(p => new Point
+            {
+                DateTime = p.DateTime,
+                Indicator = p.Value,
+                Upper = (p.Value*(100 + plusMinusPercentage))/100,
+                Lower = (p.Value*(100 - plusMinusPercentage))/100
+            });
+            return tradingBand;
+        }
+
+        public class Point
+        {
+            public DateTime DateTime { get; set; }
+            public decimal Upper { get; set; }
+            public decimal Indicator { get; set; }
+            public decimal Lower { get; set; }
         }
 
         public class Parameters
@@ -24,11 +41,17 @@ namespace Shares.Model.Indicators
             public Parameters()
             {
                 Periods = 15;
+                PlusMinusPercentage = 8;
             }
 
             [Description("The number of periods to use for calculating the moving average.")]
             [DisplayName("Periods")]
             public int Periods { get; set; }
+
+
+            [Description("The percentage offsetting the upper and lower bands from the indicator line.")]
+            [DisplayName("Plus Minus Percentage")]
+            public decimal PlusMinusPercentage { get; set; }
         }
     }
 }
